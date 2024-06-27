69f9e34ff3982e5951c9c6f97ced230b5d888ca4
MH-Shakil38 <maynuddinhsn@gmail.com>
2024-06-27 15:40:13 +0600

    createt project added in owner dashboard menu

--- resources/views/backend/layouts/include/menu/owner-menu.blade.php
+++ resources/views/backend/layouts/include/menu/owner-menu.blade.php
@@ -13,10 +13,30 @@
 {{--    </a>--}}
 {{--</li>--}}
 
+
+<ul aria-expanded="false" class="collapse  first-level">
+    <li class="sidebar-item">
+        <a href="{{route('projects.create')}}" class="sidebar-link">
+            <i class="icon-Record"></i>
+            <span class="hide-menu">Create New</span>
+        </a>
+    </li>
+    <li class="sidebar-item">
+        <a href="{{route('projects.index')}}" class="sidebar-link">
+            <i class="icon-Record"></i>
+            <span class="hide-menu">Project List</span>
+        </a>
+    </li>
+
+</ul>
+</li>
+
 <li class="sidebar-item">
     <a class="sidebar-link waves-effect waves-dark" href="{{route('total-bids')}}" aria-expanded="false">
         <i class="icon-Car-Wheel"></i>
         <span class="hide-menu">Total Bid's</span>
     </a>
 </li>
+
+
 
